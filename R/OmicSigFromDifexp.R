d29e9dcfaf017805f0d6e2507f047391260922c1
Vanessa104 <56649879+Vanessa104@users.noreply.github.com>
2021-03-31 18:17:53 -0400

    create OmicSigFromDifexp() fucntion

---
+++ R/OmicSigFromDifexp.R
@@ -0,0 +1,55 @@
+#### OmicSigFromDifexp() ####
+#' @title create an OmicSignature object from differential expsignaturession matrix
+#' updated 03/2021
+#' @description
+#' example usages:
+#'
+#' @param difexp Differential expsignaturession matrix
+#' @param metadata Metadata for the OmicSignature object. If `criteria` is `NULL`,
+#' the criterias to extract signatures will need to be provided in metadata.
+#' They can be specified in metadata fields as one or more of the followings:
+#' `logfc_cutoff`, `score_cutoff`, `adj_p_cutoff`, `p_value_cutoff`.
+#' @param criteria A character string to specify criterias used to extract
+#' signatures from difexp. e.g. "logfc > 5; score > 10". Alternatively,
+#' they can be provided in metadata fields
+#' @importFrom dplyr filter select mutate arrange pull desc %>%
+#' @importFrom rlang parse_exprs
+#' @return OmicSignature object
+#' @export
+
+OmicSigFromDifexp <- function(difexp, metadata, criteria = NULL) {
+  if (is.null(criteria)) {
+    criteria <- c()
+    if (!is.null(metadata$logfc_cutoff)) {
+      criteria <- c(criteria, paste("logfc >", metadata$logfc_cutoff))
+    }
+    if (!is.null(metadata$score_cutoff)) {
+      criteria <- c(criteria, paste("abs(score) >", metadata$score_cutoff))
+    }
+    if (!is.null(metadata$adj_p_cutoff)) {
+      criteria <- c(criteria, paste("adj_p <", metadata$adj_p_cutoff))
+    }
+    if (!is.null(metadata$p_value_cutoff)) {
+      criteria <- c(criteria, paste("p_value <", metadata$p_value_cutoff))
+    }
+    criteria <- paste(criteria, collapse = "; ")
+  }
+  cat(paste("-- criterias used to extract signatures: ", criteria, ". \n\n"))
+  v <- rlang::parse_exprs(criteria)
+  signatures <- difexp %>%
+    dplyr::filter(!!!v) %>%
+    dplyr::select(symbol, score) %>%
+    dplyr::mutate(direction = ifelse(score < 0, "-", "+")) %>%
+    dplyr::arrange(dplyr::desc(abs(score)))
+  signatures <- signatures[complete.cases(signatures), ]
+  signatures <- signatures[signatures$symbol != "", ]
+  signatures <- signatures[signatures$score != "", ]
+  signatures <- distinct(signatures, signatures$symbol, .keep_all = T)[, c(1:3)]
+
+  OmicSig <- OmicSignature$new(
+    metadata = metadata,
+    signature = signatures,
+    difexp = difexp
+  )
+  return(OmicSig)
+}
